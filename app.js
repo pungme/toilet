0de297f5de698be26e87392fe7d5c8899ffbd40d
pungme <w.manosroi@gmail.com>
2014-02-13 22:23:22 +0100

    first commit
    first commit nothing much yet...

---
+++ app.js
@@ -0,0 +1,14 @@
+//main toilet app
+
+var toiletApp = angular.module("toiletApp",[]);
+
+toiletApp.controller("toiletController", function($scope) {
+
+    $scope.data = [
+		{text: "Today is my birthday", font : "Helvetica", color :"#FFFFFF", size : 13},
+		{text: "Who cares ? ", font : "Helvetica", color :"#FFFFFF", size : 13},
+		{text: "We are nothing else but a dust in the galaxy", font : "Helvetica", color :"#FFFFFF", size : 13},
+		{text: "This toilet is worth reading than Twilight", font : "Helvetica", color :"#FFFFFF", size : 13}
+	];
+	
+});
