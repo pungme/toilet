9856064f24a4049d9b73cafbbd6393f0148b42e0
pungme <w.manosroi@gmail.com>
2014-02-14 20:51:49 +0100

    JQueryUI + angular draggable directives object tryout

--- app.js
+++ app.js
@@ -6,20 +6,30 @@ toiletApp.controller("toiletController", function($scope) {
 
     $scope.data = [
 		{text: "Today is my birthday", font : "Helvetica", color :"#FFFFFF", size : 13},
-		{text: "Who cares ? ", font : "Helvetica", color :"#FFFFFF", size : 13},
-		{text: "We are nothing else but a dust in the galaxy", font : "Helvetica", color :"#FFFFFF", size : 13},
-		{text: "This toilet is worth reading than Twilight", font : "Helvetica", color :"#FFFFFF", size : 13}
+		{text: "Who cares ? ", font : "Helvetica", color :"#fff", size : 13},
+		{text: "We are nothing else but a dust in the galaxy",font: "Helvetica", color : "#FFFFFF", size : 13},
+		{text: "This toilet is worth reading than Twilight", font : "Helvetica", color : "#FFFFFF", size : 13}
 	];
 	
 	$scope.saveText = function(){
-		//define data as an object 
+        
+        if(!$scope.data.inputText)return; //TODO: 
 		var data = {
-			text : $scope.data.inputText, 
+			text:$scope.data.inputText, 
 			font : "Helvetica", 
 			color :"#FFFFFF", 
 			size : 13
 		}	
 		$scope.data.push(data);
 	}
-	
+
+});
+
+toiletApp.directive('dragMe', function() {
+	return {
+		restrict: 'A',
+		link: function(scope, elem, attr, ctrl) {
+			elem.draggable();
+		}
+	};
 });
